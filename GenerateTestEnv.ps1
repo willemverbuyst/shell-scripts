283e00c04d0911667a40cf62de680275acfe1bf4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-02-27 19:59:39 +0100

    Generate years-months-10 files

--- GenerateTestEnv.ps1
+++ GenerateTestEnv.ps1
@@ -1,1 +1,11 @@
-1..5 | ForEach-Object {New-Item -Path . -Name "File$_.txt" -ItemType "File"}
+$months = @("JAN", 'FEB', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+$years = @(2017, 2018, 2019, 2020, 2021)
+
+ForEach ($year in $years) {
+    New-Item -Path . -Name "$year" -ItemType "Directory"
+    ForEach ($month in $months) {
+        New-Item -Path ./$year -Name "$month" -ItemType "Directory"
+        1..10 | ForEach-Object {New-Item -Path ./$year/$month -Name "File$_.txt" -ItemType "File"}
+    }
+    
+}
