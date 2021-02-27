9167f84155d7fc1f1c75dd3c0a9ada68eb888421
Willem Verbuyst <willemverbuyst@gmail.com>
2021-02-27 21:09:15 +0100

    Generate years/months/weeks/files structure

--- GenerateTestEnv.ps1
+++ GenerateTestEnv.ps1
@@ -1,11 +1,39 @@
-$months = @("JAN", 'FEB', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
-$years = @(2017, 2018, 2019, 2020, 2021)
-
-ForEach ($year in $years) {
-    New-Item -Path . -Name "$year" -ItemType "Directory"
-    ForEach ($month in $months) {
-        New-Item -Path ./$year -Name "$month" -ItemType "Directory"
-        1..10 | ForEach-Object {New-Item -Path ./$year/$month -Name "File$_.txt" -ItemType "File"}
-    }
-    
+if (Test-Path ./PROJECT) { 
+    Remove-Item -Recurse PROJECT
 }
+
+function generateFoldersAndFiles {
+    $newFolderPath = New-Item -Path . -Name "PROJECT" -ItemType Directory
+    generateFoldersForYears $newFolderPath 
+}
+
+function generateFoldersForYears ($newFolderPath) {
+    $years = @(2017, 2018)
+    ForEach ($year in $years) {
+        $yearFolderPath = New-Item -Path $newFolderPath -Name $year -ItemType Directory
+        generateFoldersForMonths $yearFolderPath
+    }
+}
+
+function generateFoldersForMonths ($yearFolderPath) {
+    $months = @("JAN", 'FEB', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+    ForEach ($month in $months) {
+        $montFolder = New-Item -Path $yearFolderPath -Name $month -ItemType Directory
+        generateWeekFolder($montFolder) 
+    }  
+}
+
+function generateWeekFolder ($monthFolder) {
+    Foreach ($i in 1..52) {
+        $weekFolderPath = New-Item -Path $monthFolder -Name "WEEK$i" -ItemType Directory
+        generateTextFiles($weekFolderPath)
+    }
+}
+
+function generateTextFiles ($weekFolderPath) {
+    Foreach ($i in 1..3) {
+       New-Item -Path $weekFolderPath -Name "FILE-$i.txt" -ItemType File
+    }
+}
+
+generateFoldersAndFiles
