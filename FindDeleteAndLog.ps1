013a43f6345065b91716e69cf841898aa828f4aa
Willem Verbuyst <willemverbuyst@gmail.com>
2021-03-02 19:21:52 +0100

    Log files

--- FindDeleteAndLog.ps1
+++ FindDeleteAndLog.ps1
@@ -1,19 +1,69 @@
 $ProjectName = "PROJECT-2"
 
+# Clean up to have a fresh start
 if (Test-Path ./$ProjectName) { 
     Remove-Item -Recurse $ProjectName
 }
 
-function generateFoldersAndFiles {
-    $NewFolderPath = New-Item -Path . -Name $ProjectName -ItemType Directory
-    generateFoldersForYears $NewFolderPath 
+function New-MainFolder {
+    $NewFolderPath = New-Item `
+        -Path . `
+        -Name $ProjectName `
+        -ItemType Directory
+
+    return $NewFolderPath
 }
 
-function generateFoldersForYears ($NewFolderPath) {
+function New-SubFoldersAndFiles {
     ForEach ($i in 1..4) {
-        $NewSubFolderPath = New-Item -Path $NewFolderPath -Name "Folder$i" -ItemType Directory
-        New-Item -Path $NewSubFolderPath.FullName -Name "File1.txt" -ItemType File
+
+        $NewSubFolderPath = New-Item `
+            -Path $NewFolderPath `
+            -Name "Folder$i" `
+            -ItemType Directory
+
+        New-Item `
+            -Path $NewSubFolderPath.FullName `
+            -Name "File1.txt" `
+            -ItemType File
     }
 }
 
-generateFoldersAndFiles
+function Get-FilesToLog {
+    $Files = Get-ChildItem `
+    -Path ./$ProjectName `
+    -Filter "File1.txt" `
+    -Recurse
+
+    # For development
+    Write-Host ($Files | Out-String) -ForegroundColor red
+
+    Return $Files
+}
+
+function Write-Message ($NewFolderPath, $LogMessage) {
+    if (-not (Test-Path $NewFolderPath/LOGS)) { 
+        New-Item `
+            -Path $NewFolderPath `
+            -Name 'LOGS' `
+            -ItemType Directory
+    }
+
+    New-Item `
+        -Path $NewFolderPath `
+        -Name "Log.txt" `
+        -ItemType File
+
+    Add-Content `
+        -Path $NewFolderPath/Log.txt `
+        -Value $LogMessage
+}
+
+
+$NewFolderPath = New-MainFolder
+New-SubFoldersAndFiles $NewFolderPath 
+$Files = Get-FilesToLog
+Write-Message $NewFolderPath $Files
+
+
+
