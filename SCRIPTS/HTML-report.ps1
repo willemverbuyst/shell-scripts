d7da0ca926a817e8b6c26af3e11aab4afcda972e
Willem Verbuyst <willemverbuyst@gmail.com>
2021-03-26 07:11:26 +0100

    Separate css, use Head

--- SCRIPTS/HTML-report.ps1
+++ SCRIPTS/HTML-report.ps1
@@ -1,11 +1,35 @@
-s$obj = [PSCustomObject]@{
+$header = @"
+
+    <style>
+
+        table, 
+        th, 
+        td {
+            border-collapse: collapse; 
+            border: 1px solid teal; 
+            padding: 5px;
+        } 
+        
+        h1 {
+            color: teal
+        }
+
+    </style>
+
+"@
+
+$person = [PSCustomObject]@{
     firstName = 'Sjaak'
     lastName = 'Marty'
     age = 34
 }
+$titleDoc = "<title>$($person.firstName)</title>"
 
-$style = "<style>table, th, td {border-collapse: collapse; border: 1px solid red; padding: 5px;}</style>"
+$titleSection = "<h1>Report of Person</h1>"
+$mainSection = $person | ConvertTo-Html -As List -Fragment
 
-$obj | ConvertTo-Html -Head $style | Out-File ./index.html -Force
+$report = ConvertTo-Html -Body "$titleSection $mainSection"  -Head "$header $titleDoc"
+ 
+$report | Out-File ./index.html -Force
 
 Invoke-Expression ./index.html
